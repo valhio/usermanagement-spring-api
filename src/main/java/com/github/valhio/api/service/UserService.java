54f75b38cd35128abe38cfb3c065b9903fe17c9d
valhio <valentingerasimow@gmail.com>
2022-12-16 16:07:32 +0200

    Removed unused imports

--- src/main/java/com/github/valhio/api/service/UserService.java
+++ src/main/java/com/github/valhio/api/service/UserService.java
@@ -1,12 +1,9 @@
 package com.github.valhio.api.service;
 
 import com.github.valhio.api.exception.domain.EmailExistException;
-import com.github.valhio.api.exception.domain.UserNotFoundException;
 import com.github.valhio.api.exception.domain.UsernameExistException;
 import com.github.valhio.api.model.User;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.Collection;
 
 public interface UserService {
@@ -17,7 +14,6 @@ public interface UserService {
     public User findUserByUsername(String username);
 
     public User findUserByEmail(String email);
-
 
     User login(User user);
 }
