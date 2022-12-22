35a356d10846d4d7c5ac7ea3c626c6cc0913e7d9
valhio <valentingerasimow@gmail.com>
2022-12-22 14:15:23 +0200

    SecurityContextHolder.getContext() was causing an error

--- src/main/java/com/github/valhio/api/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/github/valhio/api/filter/JWTAuthorizationFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -24,10 +23,9 @@ import static com.github.valhio.api.constant.SecurityConstant.TOKEN_PREFIX;
  *   This class is used to validate the JWT token sent by the client in the Authorization header of the request.
  * */
 
-@NoArgsConstructor
 @Component
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
-    private JWTTokenProvider jwtTokenProvider;
+    private final JWTTokenProvider jwtTokenProvider;
 
     public JWTAuthorizationFilter(JWTTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
@@ -38,7 +36,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         if (request.getMethod().equalsIgnoreCase(OPTIONS_HTTP_METHOD)) { // if the request is OPTIONS, then we allow it
             response.setStatus(HttpStatus.OK.value()); // we set the status to OK
         } else { // if the request is not OPTIONS, then we check if the request has a token
-            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION); // we get the authorization header
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION); // we get the "Authorization" header field
 
             // if the header is null or doesn't start with Bearer, we pass the request to the next filter
             if (authorizationHeader == null || !authorizationHeader.startsWith(TOKEN_PREFIX)) {
@@ -50,7 +48,8 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             String username = jwtTokenProvider.getSubject(token); // we get the username from the token
 
             // if the username is not null and the security context is empty, we set the authentication
-            if (jwtTokenProvider.isTokenValid(username, token) && SecurityContextHolder.getContext().getAuthentication() == null) {
+//            if (jwtTokenProvider.isTokenValid(username, token) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtTokenProvider.isTokenValid(username, token)) {
                 List<? extends GrantedAuthority> authorities = jwtTokenProvider.getAuthorities(token); // we get the authorities from the token
                 Authentication authentication = jwtTokenProvider.getAuthentication(username, authorities, request); // we get the authentication from the token
                 SecurityContextHolder.getContext().setAuthentication(authentication); // we set the authentication in the security context
