2bb3fc2105b3b503a04cb916254a270936d217e4
valhio <valentingerasimow@gmail.com>
2023-01-04 21:31:38 +0200

    Added Cross-Origin Resource Sharing

--- src/main/java/com/github/valhio/api/config/SecurityConfiguration.java
+++ src/main/java/com/github/valhio/api/config/SecurityConfiguration.java
@@ -50,6 +50,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors().and()
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
                 .exceptionHandling()
