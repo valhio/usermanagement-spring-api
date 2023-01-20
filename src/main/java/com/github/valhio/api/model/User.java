f97726d5fa912ef0ab9325147d7d1284b36f2bdb
valhio <valentingerasimow@gmail.com>
2023-01-20 20:02:01 +0200

    Added back the field validation

--- src/main/java/com/github/valhio/api/model/User.java
+++ src/main/java/com/github/valhio/api/model/User.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.valhio.api.enumeration.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -24,32 +27,34 @@ public class User extends Auditable<String> implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Long id;
 
     private String userId;
 
     private String username;
 
-    //    @NotNull(message = "Password cannot be empty")
-//    @Length(min = 7, message = "Password should be atleast 7 characters long")
+    @NotNull(message = "Password cannot be empty")
+    @Length(min = 7, message = "Password should be atleast 7 characters long")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
-    //    @NotNull(message = "First Name cannot be empty")
+    @NotNull(message = "First Name cannot be empty")
     @Column(name = "first_name")
     private String firstName;
 
     @Column(name = "middle_name")
     private String middleName;
 
-    //    @NotNull(message = "Last Name cannot be empty")
+    @NotNull(message = "Last Name cannot be empty")
     @Column(name = "last_name")
     private String lastName;
 
-    //    @NotNull(message = "Email cannot be empty")
-//    @Email(message = "Please enter a valid email address")
+    @NotNull(message = "Email cannot be empty")
+    @Email(message = "Please enter a valid email address")
     private String email;
 
-    //    @Length(min = 10, message = "Password should be atleast 10 number long")
+    @Length(min = 10, message = "Password should be at least 10 number long")
     private String phone;
 
     private String address;
